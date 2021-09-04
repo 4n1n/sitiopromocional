32382901cd50086e89c8b08aa6d6fba59b658c28
ana <anamleyes@gmail.com>
2021-09-03 23:02:38 -0300

    Primeras consultas en Dashboard

--- tienda/models.py
+++ tienda/models.py
@@ -4,31 +4,35 @@ from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
+
 class CATEGORIES(models.Model):
-    #id_categoria=models.AutoField(primary_key=True)
-    descripcion=models.CharField(null=False,default="",max_length=200)
-    #cantidad_de_articulos=models.PositiveIntegerField(null=False)
+    # id_categoria=models.AutoField(primary_key=True)
+    descripcion = models.CharField(null=False, default="", max_length=200)
+    # cantidad_de_articulos=models.PositiveIntegerField(null=False)
+
     def __str__(self):
         return self.descripcion
 
+
 class DISCOUNT(models.Model):
-    #id_descuento=models.AutoField(primary_key=True)
-    descripcion=models.CharField(null=False,default="",max_length=200)
-    porcentaje=models.DecimalField(null=False,default=0,max_digits=8,decimal_places=2)
+    # id_descuento=models.AutoField(primary_key=True)
+    descripcion = models.CharField(null=False, default="", max_length=200)
+    porcentaje = models.DecimalField(null=False, default=0, max_digits=8, decimal_places=2)
+
     def __str__(self):
-        #Formateo de texto para mostrar porcentaje entre paréntesis.
-        texto="{0}({1})"
-        #Mostramos desc y %.
-        return texto.format(self.descripcion,self.porcentaje)
+        # Formateo de texto para mostrar porcentaje entre paréntesis.
+        texto = "{0}({1})"
+        # Mostramos desc y %.
+        return texto.format(self.descripcion, self.porcentaje)
 
 
 class PRODUCTS(models.Model):
-    #id_producto=models.AutoField(primary_key=True)
-    descripcion=models.CharField(max_length=50) 
-    precio=models.DecimalField (max_digits=10,decimal_places=2)
-    proveedor=models.CharField(max_length=50)
-    categoria=models.ForeignKey(CATEGORIES,on_delete=models.CASCADE)
-    descuento=models.ForeignKey(DISCOUNT,on_delete=models.CASCADE)
+    # id_producto=models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    proveedor = models.CharField(max_length=50)
+    categoria = models.ForeignKey(CATEGORIES, on_delete=models.CASCADE)
+    descuento = models.ForeignKey(DISCOUNT, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.descripcion
--- tienda/views.py
+++ tienda/views.py
@@ -3,40 +3,59 @@ from django.http.request import QueryDict
 from tienda.models import CATEGORIES, PRODUCTS, DISCOUNT
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Count,OrderBy,Min,Max,Avg
+from django.db.models import Count, OrderBy, Min, Max, Avg
 # Create your views here.
 
-def inicio (request):
+
+def inicio(request):
     return render(request, 'tienda/inicio.html', {})
 # Comento la def simple
-#def productos(request):
-#    return render(request, 'tienda/productos.html', {}) 
+# def productos(request):
+#    return render(request, 'tienda/productos.html', {})
+
+
 def productos(request):
-    #Creo variable donde almaceno todos los objetos PRODUCTS
-    listaproductos=PRODUCTS.objects.all()
-    #La vista productos va a devolver en la url registrada, un objeto oproductos con el contenido de la variable listaproductos
-    return render(request, 'tienda/productos.html', {"oproductos":listaproductos})
+    # Creo variable donde almaceno todos los objetos PRODUCTS
+    listaproductos = PRODUCTS.objects.all()
+    # La vista productos va a devolver en la url registrada, un objeto oproductos con el contenido de la variable listaproductos
+    return render(request, 'tienda/productos.html', {"oproductos": listaproductos})
 
-def detalle (request):
+
+def detalle(request):
     return render(request, 'tienda/detalles.html', {})
-    
-def dashboard (request):
-    prodxcat=PRODUCTS.objects.all()
-    return render(request, 'tienda/dashboard.html', {})
 
-def prueba (request):
-    #productoscaros=PRODUCTS.objects.all().order_by('precio') [:3]   
-    #return render(request,'tienda/prueba.html', {"oproductoscaros":productoscaros})
-    #variables Vista Categorías por Cant. de Productos
+
+def dashboard(request):
+    datos = {}
+    categorias = CATEGORIES.objects.all()
+    productos = PRODUCTS.objects.all()
+    categorias_productos = {}
+    for categoria in categorias:
+        categorias_productos[categoria.descripcion] = 0
+    # print(categorias_productos)
+    for producto in productos:
+        if (producto.categoria.descripcion in categorias_productos):
+            categorias_productos[producto.categoria.descripcion] += 1
+    # print(categorias_productos)
+    datos['categorias'] = categorias
+    datos['productos'] = productos
+    datos['categorias_productos'] = categorias_productos
+    return render(request, 'tienda/dashboard.html', {"datos": datos})
+
+
+def prueba(request):
+    #productoscaros=PRODUCTS.objects.all().order_by('precio') [:3]
+    # return render(request,'tienda/prueba.html', {"oproductoscaros":productoscaros})
+    # variables Vista Categorías por Cant. de Productos
     cat = CATEGORIES.objects.all()
     nro_prod = CATEGORIES.objects.annotate(Count('products'))
     vnro_prod = vars(nro_prod)
-    #variables Vista Ofertas por Cant. de Productos
-    prod_tipodeoferta=DISCOUNT.objects.annotate(Count('products'))
-    ofertaprod=DISCOUNT.objects.all()
-    #variables Vista Categoría con su Máximo Descuento.
-    discxcat=CATEGORIES.objects.annotate(Max('products__descuento__porcentaje')).OrderBy(-'products__descuento__porcentaje')
-    #envío todos los objetos al template
-    return render(request,'tienda/prueba.html', {"ocat":cat,
-                "onroproductos":nro_prod,
-                "otipof":prod_tipodeoferta,"oofert":ofertaprod})
+    # variables Vista Ofertas por Cant. de Productos
+    prod_tipodeoferta = DISCOUNT.objects.annotate(Count('products'))
+    ofertaprod = DISCOUNT.objects.all()
+    # variables Vista Categoría con su Máximo Descuento.
+    discxcat = CATEGORIES.objects.annotate(Max('products__descuento__porcentaje')).OrderBy(-'products__descuento__porcentaje')
+    # envío todos los objetos al template
+    return render(request, 'tienda/prueba.html', {"ocat": cat,
+                                                  "onroproductos": nro_prod,
+                                                  "otipof": prod_tipodeoferta, "oofert": ofertaprod})
